fb0ed45f13e4a455c626803534208111842769a1
LinmeiJ <linmeijiang@hotmail.com>
2019-04-27 23:28:54 -0400

    third commit

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -1,122 +1,115 @@
+
 import java.util.Scanner;
 
 /******************** Author: Linmei Mills *********************************/
 public class PigLatin {
 	static Scanner sc = new Scanner(System.in);
-	public static void main(String[] args) {	
-		boolean isUpper, isAllUpper;
-		String userStr;
-		String[] ch = {"a", "e", "i", "o", "u"};
+	
+	public static void main(String[] args) {
+//		boolean isUp;
+//		boolean isAllUp;
+		String userInput;
 		System.out.println("Welcome to the pig Latin Translator! \n");
 		
 	//while loop to ask use whether contunue
-		String userInput = "y";
-		while(userInput.equalsIgnoreCase("y")) {
-		
-			userStr = getStrAndcheckEmptyStr();
-			String[] splited = userStr.split("\\s+");
-			String output = "";
-			String plStr;
-			
-			// splitting string and send each word to get translated into Pig Latin.
-			for(int i = 0; i < splited.length; i++) {
-				isUpper = Character.isUpperCase(splited[i].charAt(0));
-				isAllUpper =splited[i].toUpperCase().equals(splited[i]);
-	
-				plStr = caseKeeper(splited[i], ch, isUpper, isAllUpper);	
-				output += plStr + " ";
-			}
-			System.out.println("Pig Latin is: " + output);
-			
-			System.out.print("\nContinue(y/n): ");
+		String userContinue = "y";
+		while(userContinue.equalsIgnoreCase("y")) {
+			System.out.println("Enter a line to be translated: ");
 			userInput = sc.nextLine();
-			System.out.println();
+			String punctuation = "";
+			 //comma or period
+			
+			String[] arr = userInput.split(" ");
+			String pigLatin = "";
+			for(int i = 0; i < arr.length; i++) {
+				punctuation= checkPunctuation(arr[i]);
+				arr[i] = removePunctuation(arr[i]);
+				String word = checkLetterCase(userInput, arr[i]);
+				pigLatin += word + " "; 
+			}
+			 // check if the last index is empty
+			pigLatin = pigLatin.substring(0, pigLatin.length()); // remove the last space
+			
+			pigLatin = pigLatin.substring(0, pigLatin.length());
+			System.out.println(pigLatin + punctuation);
+			
+			System.out.println("Translate another line? (y/n): ");
+			userContinue = sc.nextLine();
 		}
 		System.out.println("Goodbye!");
 	}
 	
-	//slice out comma and period
-	public static String getCommaAndPeriod(String splitStr) {
-		String strEnd;
-		if(splitStr.contains(".")) {
-			strEnd = ".";
-			splitStr = splitStr.substring(0, splitStr.length());
-			
-		}else if(splitStr.contains(",")) {
-			strEnd = ",";
-			splitStr =splitStr.substring(0, splitStr.length());
+	public static String checkPunctuation(String userStr) {
+		String endStr;
+		if((userStr.substring(userStr.length() - 1).contentEquals("."))){ //not contain, check the word for ech ending!!!
+			endStr= ".";
+		}else if(userStr.contains(",")) {
+			endStr = ",";
 		}else {
-			strEnd = "";
+			endStr = "";
 		}	
-		return strEnd;
+		return endStr;
 	}
 	
-	//tanslate string to pig Latin
-	public static String convertToPigLatin(String str, String[] chr, boolean isUpper, boolean isAllUpper) {
+	public static String removePunctuation(String userStr) {
+		if(userStr.contains(".")) {
+			return userStr = userStr.substring(0, userStr.length()-2);	
+		}else if(userStr.contains(",")) {
+			return userStr = userStr.substring(0, userStr.length()-2);
+		}else {
+			return userStr;
+		}
+	}
+	
+	/*****************String method:  checkLetterCase() ***************************/
+	public static String checkLetterCase(String userStr, String word) {
+		String pigLatin;
+		if((userStr.toUpperCase()).equals(userStr)){
+			pigLatin = translateStr(word).toUpperCase();
+			return pigLatin;
+		}
+		
+		else if(Character.isUpperCase(userStr.charAt(0))){
+			pigLatin = translateStr(word);
+			return pigLatin.substring(0,1).toUpperCase() + pigLatin.substring(1);
+		}
+		
+		return pigLatin = translateStr(word).toLowerCase();
+	}
+	
+	public static String translateStr(String word) {
 		final String AY = "ay";
 		final String WAY = "way";
-		int strIndex; 
-		String newStr, strPart1, strPart2;
-		String strEnd = getCommaAndPeriod(str);
-		str = str.toLowerCase();
-		for(int i = 0; i < str.length(); i++) {
-			
-			for(int j = 0; j < chr.length; j++) {
-				String stri = Character.toString(str.charAt(i));
-				String strj = chr[j];
-				
-				if(stri.equalsIgnoreCase(strj) && i == 0) {  
-						newStr = str.concat(WAY);
-						
-						return newStr + strEnd;
+		String[] vowels = {"a", "e", "i", "o", "u"};
+		String newStr;
+		word = word.toLowerCase(); //convert string to lower case
+		for(int i =0; i < word.length(); i++) {
+			for(int j = 0; j < vowels.length; j++) {
+				String letter = Character.toString(word.charAt(i));
+				String vowel = vowels[j];
+				if(letter.equalsIgnoreCase(vowel) && i == 0) {
+					return newStr = word.concat(WAY);
 				}
-				else if(stri.equalsIgnoreCase(strj)){ 
-						strIndex = i;
-						strPart1 = str.substring(0,strIndex);
-						strPart2 = str.substring(strIndex);
-						newStr = strPart2 + strPart1 + AY + strEnd;
-						return newStr;
+				else if(letter.equalsIgnoreCase(vowel)) {
+					int letterIndex = i;
+					String wordSplitL= word.substring(0, letterIndex + 1);
+					String wordSplitR= word.substring(letterIndex);
+					return newStr =  wordSplitR + wordSplitL + AY;
 				}
 			}
 		}
-		newStr = str.substring(1)+ str.charAt(0)+ AY + strEnd;
-		return newStr;
+		return newStr = word.substring(1) + word.charAt(0) + AY ;
 	}
-	
-	/* Thie method is for making the Pig Latin word has the same capitalization pattern as from the orginal string.
-	 * Returns a new string that is followed the capitalization pattern. 
-	 * Note: Only If the first letter of the string user entered is in capital or the whole string is 
-	 *	     in capital, then the translated Pig Latin follows the same capitalization rule(start w/
-	 *		 a capital letter or all capital letters). Otherwise, all translated as lower case Pig Latin since
-	 * 		 there is no specification to it (on the extended challenges).
-	 */
-	public static String caseKeeper(String str, String[] chr, boolean isUpper, boolean isAllUpper) {
-		String newStrCased;
-		newStrCased = convertToPigLatin(str, chr, isUpper, isAllUpper);
-		
-		if(isAllUpper){
-			return newStrCased.toUpperCase();
-		}
-		else if(isUpper) {
-			return newStrCased.substring(0,1).toUpperCase() + newStrCased.substring(1);
-		}
-		else
-			return newStrCased;
-	}
-	
-	
-	//get user input
-	public static String getStrAndcheckEmptyStr() {
-		String userS;
-		System.out.println("Enter a line to be translated: ");
-		userS = sc.nextLine();
-		while(userS.isEmpty()) {
-			System.out.println("Entry is empty, try again: ");
-			userS = sc.nextLine();
-			continue;
-		}
-		return userS;
-	}
-	
 }
-	
+
+
+
+
+
+
+
+
+
+
+
+
